a1cef15b705c9e262a83b5bf4bcc3e6c7dd65503
achrafib1 <achibbuisness512@gmail.com>
2024-12-22 23:05:58 +0100

    Implement core chat functionality and improve agent capabilities
     - Add `chroma_db/` to `.gitignore` to prevent versioning the database.
     - Modify `app.py` to handle chat interactions with the assistant and
       make the agent the responsible for extracting the CV and Job
       Description.
     - Update `src/cv_helper_agents/config/agents.yaml` to reflect the new
       agent approach.
     - Update `src/cv_helper_agents/config/tasks.yaml` to define the
       task for the new agent logic.
     - Modify `src/cv_helper_agents/crew.py` to implement the crew for
       the assistant agent and enable crew memory.

--- src/app.py
+++ src/app.py
@@ -1,1 +1,66 @@
-# main app file
+# # main app file
+# from cv_helper_agents.crew import CvHelper
+import warnings
+
+# warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# CvHelper_crew = CvHelper()
+# while True:
+#     user_input = input("Please enter your input (or type 'exit' to quit):\n user:  ")
+#     if user_input.lower() == "exit":
+#         print("Exiting the chat. Goodbye!")
+#         break
+#     user_input_dict = {"user_input": user_input}
+#     res = CvHelper_crew.crew().kickoff(inputs=user_input_dict)
+#     print(f"Output:\n agent:  {res}")
+
+from cv_helper_agents.crew import CVHelper
+from mem0 import Memory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = {
+      "api_version": "v1.1",
+    "vector_store": {
+        "provider": "chroma",
+        "config": {
+            "collection_name": "chatbot_memory",
+            "path": "./chroma_db",
+        },
+    },
+}
+
+memory = Memory.from_config(config) 
+
+
+def run():
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit", "bye"]:
+            print("Chatbot: Goodbye! It was nice talking to you.")
+            break
+
+        # Add user input to memory
+        memory.add(f"User: {user_input}", user_id="Lennex")
+
+        # Retrieve all user and assistant messages for the current conversation
+        user_messages = memory.get_all(user_id="Lennex")
+        assistant_messages = memory.get_all(user_id="Assistant")
+
+        # Combine and format context from both user and assistant memories
+        context = "\\n".join([message["memory"] for message in user_messages] + [message["memory"] for message in assistant_messages])
+
+
+        inputs = {
+            "user_message": f"{user_input}",
+            "context": f"{context}",
+        }
+
+        response = CVHelper().crew().kickoff(inputs=inputs)
+
+        # Add chatbot response to memory
+        memory.add(f"Assistant: {response}", user_id="Assistant")
+        print(f"Assistant: {response}")
+
+run()
